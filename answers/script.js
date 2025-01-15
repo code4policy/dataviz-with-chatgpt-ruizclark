fb6120c140ce74410166405293ff4112c3c8aedb
Ruiz Clark <b.ruiz.clark@gmail.com>
2025-01-15 10:49:27 -0500

    Show only top 10 with option to view all

--- answers/script.js
+++ answers/script.js
@@ -6,58 +6,79 @@ d3.csv("311_boston_data.csv").then(data => {
     // Sort the data in descending order based on Count
     data.sort((a, b) => b.Count - a.Count);
 
-    // Set dimensions and margins (increase right margin for space)
+    // Set dimensions and margins
     const margin = { top: 20, right: 50, bottom: 40, left: 200 };
     const width = 800 - margin.left - margin.right;
-    const height = data.length * 25;
 
     // Create the SVG canvas
     const svg = d3.select("#chart")
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+        .attr("width", width + margin.left + margin.right);
 
-    // Create scales
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.Count)])
-        .range([0, width]);
+    // Function to draw the chart
+    const drawChart = (filteredData) => {
+        const height = filteredData.length * 25 + margin.top + margin.bottom;
 
-    const y = d3.scaleBand()
-        .domain(data.map(d => d.reason))
-        .range([0, height])
-        .padding(0.1);
+        svg.attr("height", height);
 
-    // Add axes
-    svg.append("g")
-        .call(d3.axisLeft(y));
+        // Clear existing chart content
+        svg.selectAll("*").remove();
 
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x).ticks(5));
+        // Create scales
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(filteredData, d => d.Count)])
+            .range([0, width]);
 
-    // Create bars
-    svg.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("y", d => y(d.reason))
-        .attr("height", y.bandwidth())
-        .attr("x", 0)
-        .attr("width", d => x(d.Count));
+        const y = d3.scaleBand()
+            .domain(filteredData.map(d => d.reason))
+            .range([margin.top, height - margin.bottom])
+            .padding(0.1);
 
-    // Add labels
-    svg.selectAll(".label")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("class", "label")
-        .attr("y", d => y(d.reason) + y.bandwidth() / 2)
-        .attr("x", d => x(d.Count) + 5)
-        .attr("dy", ".35em")
-        .style("font-size", "12px")
-        .text(d => d.Count);
+        // Add axes
+        svg.append("g")
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y));
+
+        svg.append("g")
+            .attr("transform", `translate(${margin.left},${height - margin.bottom})`)
+            .call(d3.axisBottom(x).ticks(5));
+
+        // Create bars
+        svg.selectAll(".bar")
+            .data(filteredData)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", margin.left)
+            .attr("y", d => y(d.reason))
+            .attr("height", y.bandwidth())
+            .attr("width", d => x(d.Count))
+            .style("fill", "maroon");
+
+        // Add labels
+        svg.selectAll(".label")
+            .data(filteredData)
+            .enter()
+            .append("text")
+            .attr("class", "label")
+            .attr("x", d => margin.left + x(d.Count) + 5)
+            .attr("y", d => y(d.reason) + y.bandwidth() / 2)
+            .attr("dy", ".35em")
+            .text(d => d.Count);
+    };
+
+    // Initial rendering with the top 10 issues
+    const top10Data = data.slice(0, 10);
+    drawChart(top10Data);
+
+    // Toggle functionality
+    const toggleButton = d3.select("#toggle-chart");
+    let showingAll = false;
+
+    toggleButton.on("click", () => {
+        showingAll = !showingAll;
+        drawChart(showingAll ? data : top10Data);
+        toggleButton.text(showingAll ? "Show Top 10" : "Show All");
+    });
 });
 
